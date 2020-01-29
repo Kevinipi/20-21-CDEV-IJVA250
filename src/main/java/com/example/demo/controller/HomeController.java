48b1b97556015858b064bfdcf2e227661f9c2404
Kevinipi <kevin.pose@ipilyon.net>
2020-01-29 16:36:30 +0100

    Premier export des clients dans un fichier CSV

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,10 +6,17 @@ import com.example.demo.entity.Facture;
 import com.example.demo.service.impl.ClientServiceImpl;
 import com.example.demo.service.ArticleService;
 import com.example.demo.service.FactureService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.lang.model.element.Name;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -18,8 +25,11 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
     private ArticleService articleService;
+    @Autowired
     private ClientServiceImpl clientServiceImpl;
+    @Autowired
     private FactureService factureService;
 
     public HomeController(ArticleService articleService, ClientServiceImpl clientService, FactureService factureService) {
@@ -42,5 +52,18 @@ public class HomeController {
         modelAndView.addObject("factures", factures);
 
         return modelAndView;
+    }
+
+    @GetMapping("/clients/csv")
+    public void clientsCSV (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=\"export-articles.csv\"");
+        PrintWriter writer = response.getWriter();
+        List<Client> clients = clientServiceImpl.findAllClients();
+        for(int i = 0; i < clients.size(); i++) {
+            Client client = clients.get(i);
+            String ligne = client.getNom() +";"+ client.getPrenom();
+            writer.println(ligne);
+        }
     }
 }
